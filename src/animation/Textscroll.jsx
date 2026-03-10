6a515ef32158aeb86c40fdb864a507701ac69337
SANTHOSH MATHAVAN NT961 <santhoshmathavan.nt961@kpriet.ac.in>
2026-03-10 17:12:10 +0530

    update

--- src/animation/Textscroll.jsx
+++ src/animation/Textscroll.jsx
@@ -7,17 +7,17 @@ const Textscroll = () => {
       <div className="scroll-text flex whitespace-nowrap font-bold uppercase
       text-[100px] sm:text-[120px] md:text-[150px] lg:text-[300px] gap-10 opacity-50">
 
-        <span>Hemello</span>
+        <span>Hemelo</span>
         <span>Cotton</span>
         <span>Global</span>
-        <span>Hemello</span>
+        <span>Hemelo</span>
         <span>Cotton</span>
         <span>Global</span>
 
-         <span>Hemello</span>
+         <span>Hemelo</span>
         <span>Cotton</span>
         <span>Global</span>
-        <span>Hemello</span>
+        <span>Hemelo</span>
         <span>Cotton</span>
         <span>Global</span>
 
